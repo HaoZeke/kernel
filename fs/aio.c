f3bf8b277a6f830ff0457aa01f0066fe70d41bca
flar2 <asegaert@gmail.com>
2013-11-06 23:24:29 -0500

    AIO: Don't plug the I/O queue in do_io_submit()
    Signed-off-by: flar2 <asegaert@gmail.com>

--- fs/aio.c
+++ fs/aio.c
@@ -1161,6 +1161,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 	struct blk_plug plug;
 #endif
 
+
 	if (unlikely(nr < 0))
 		return -EINVAL;
 
@@ -1179,6 +1180,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 #ifndef CONFIG_AIO_SSD_ONLY
 	blk_start_plug(&plug);
 #endif
+
 
 	/*
 	 * AKPM: should this return a partial result if some of the IOs were
